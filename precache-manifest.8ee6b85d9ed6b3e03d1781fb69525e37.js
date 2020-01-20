81fac8b211863d8cf1aca436d252b03766b53e54
Kiran Mehetre <Kiranm@WinjitTechnologiesPvtLtd.local>
2020-01-20 16:45:25 +0530

    commit changes

---
+++ precache-manifest.8ee6b85d9ed6b3e03d1781fb69525e37.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e11cfbb8f8e5c26b66ac5c92bd52157",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7db656bb8cfd821e811a",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "aa21d6c728e730de6739",
+    "url": "/static/js/2.75cab5e0.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.75cab5e0.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7db656bb8cfd821e811a",
+    "url": "/static/js/main.fdc9c7a2.chunk.js"
+  },
+  {
+    "revision": "c6535a732c9026b327df",
+    "url": "/static/js/runtime-main.39a9aa13.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
